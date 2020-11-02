d17f0c712d06f8da26fb5be9002471ad3b727820
Toricruz Mendiola <mtoricruz@gmail.com>
2020-11-02 13:30:01 -0800

    added sol to arrayMaxConsecutiveSum2.py

--- CodeSignal/python/commontechniques/arrayMaxConsecutiveSum2.py
+++ CodeSignal/python/commontechniques/arrayMaxConsecutiveSum2.py
@@ -1,9 +1,30 @@
 # Understanding
-#
+# loop through list
+# currMax and update as new possible contiguous sum is found
+# 
 
 # Planning
-#
+# currMax = 0
+# for i in inputArray:
+    # tempSum = i + i[i+1]
+    # if currMax > tempSum:
+        # continue
+    # else:
+        # currMax = tempSum
+# return tempSum
 
 # Execution
 
 # Refactor/Reflection
+def arrayMaxConsecutiveSum2(inputArray):
+    currSum = 0
+    maxSum = inputArray[0]
+    for i in inputArray:
+        tempSum = i + currSum
+        if tempSum > 0:
+            currSum = tempSum
+        else:
+            currSum = 0
+        if tempSum > maxSum:
+            maxSum = tempSum
+    return maxSum
