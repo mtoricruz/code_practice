921b6ac24b29b43fa26ff9ba97c2cc3deb90e621
Toricruz Mendiola <mtoricruz@gmail.com>
2020-11-02 10:07:07 -0800

    added more solutions from interview practice sets

---
+++ CodeSignal/python/dftandbfs/traverseTree.py
@@ -0,0 +1,13 @@
+# Understanding
+# Create iterative solution(no recursion)
+# no restriction space complexity
+# use list to store nodes throughout traversal
+# rightPoint/ leftPoint/ rightPrev/ left Prev
+# BFS since we will move through each level
+
+# Planning
+#
+
+# Execution
+
+# Refactor/Reflection
