a4e1e386488fcd274277ec0240d8628301c41e9e
Toricruz Mendiola <mtoricruz@gmail.com>
2020-11-05 13:11:28 -0800

    fillingBlocks.py and largestValuesInTreeRows.py sol added

--- CodeSignal/python/dfsandbfs/largestValuesInTreeRows.py
+++ CodeSignal/python/dfsandbfs/largestValuesInTreeRows.py
@@ -1,2 +1,53 @@
 # Understanding
-# 
+# BFS to find largest node
+# initialize q = [t]
+# return array of largest val in each row
+    # initialize a results arr + levelNodes arr
+    # depth counter
+# each level we can append node.value to levelNodes arr
+# results.append(max(levelNodes))
+# while q:
+    # if t.left == None and t.right == None:
+        # return results
+
+# Planning
+# results, levelNodes = [], []
+# q = [t]
+# depth = 0
+# if not t:
+    # return []
+
+# while q:
+    # if t.right:
+        # levelNodes.append(t.right)
+    # if t.left:
+        # levelNodes.append(t.left)
+    # depth += 1
+# while depth > 0:
+    # results.append(max(levelNodes))
+    # levelNodes = []
+# return results
+
+
+# Execute
+
+# Refactor/Reflection
+def largestValuesInTreeRows(t):
+    if not t:
+        return []
+    results, levelNodes = [], [t]
+    while levelNodes:
+        temp = []
+        maxVal = levelNodes[0].value
+        for node in levelNodes:
+            if node.value > maxVal:
+                maxVal = node.value
+            if node.left:
+                temp.append(node.left)
+            if node.right:
+                temp.append(node.right)
+        levelNodes = temp
+        results.append(maxVal)
+    return results
+
+# learned there is no need for depth counter
